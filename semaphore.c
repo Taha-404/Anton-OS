233892121e739e5f4aa5cac3b8f530a5d41e500c
youssef.taha724@gmail.com <youssef.taha724@gmail.com>
2020-10-24 01:28:54 +0200

    finished semaphores implementation

---
+++ semaphore.c
@@ -0,0 +1,106 @@
+/**********************************************************************************
+ *
+ * File Name: semaphore.c
+ *
+ * Description: Source file for the semaphores API functions
+ *
+ * Engineer: Youssef Taha
+ *
+***********************************************************************************/
+
+#include"semaphore.h"
+
+/**********************************************************************************
+ *                      Functions Definitions                                  *
+***********************************************************************************/
+
+/***********************************************************************************
+* [Function Name]: SemaphoreCreateBinary
+*
+* [Description]:   creating binary semaphore
+*
+* [Arguments]:	   handle of the semaphore
+*
+* [Retuen]:		   	 void
+***********************************************************************************/
+
+void SemaphoreCreateBinary( SemaphoreHandle_t * semaphore )
+{
+
+	__disable_irq();
+	/*initialize the counter value with 1*/
+	*semaphore=1;
+	__enable_irq(); 
+
+}
+
+/***********************************************************************************
+* [Function Name]: SemaphoreCreateCounting
+*
+* [Description]:   creating counting semaphore
+*
+* [Arguments]:	   handle of the semaphore, blocking time of the task
+*
+* [Retuen]:		   	 void
+***********************************************************************************/
+
+void SemaphoreCreateCounting( SemaphoreHandle_t * semaphore , uint32_t MaxCount)
+{
+
+	__disable_irq();
+	/*initialize the counter value with MaxCount*/
+	*semaphore=MaxCount;
+	__enable_irq(); 
+
+}
+
+/***********************************************************************************
+* [Function Name]: SemaphoreTake
+*
+* [Description]:   taking semaphore
+*
+* [Arguments]:	   handle of the semaphore, blocking time of the task
+*
+* [Retuen]:		   	 void
+***********************************************************************************/
+
+void SemaphoreTake( SemaphoreHandle_t * semaphore,uint32_t TicksToWait )
+{
+	/*if the semaphore is taken delay the thread with TicksToWait value */
+	while(*semaphore==0)
+	{
+		TaskDelay(TicksToWait,(uint32_t)currentPtr);
+	}
+	__disable_irq();
+	/*if the semaphore is given decrement the counter then continue*/
+	*semaphore=*semaphore-1;
+	__enable_irq(); 
+}
+
+/***********************************************************************************
+* [Function Name]: SemaphoreGive
+*
+* [Description]:   giving semaphore
+*
+* [Arguments]:	   handle of the semaphore
+*
+* [Retuen]:		   	 void
+***********************************************************************************/
+
+void SemaphoreGive( SemaphoreHandle_t * semaphore )
+{
+
+	__disable_irq();
+	/*increment the counter*/
+	*semaphore=*semaphore+1;
+	/*ask for context switch*/
+	taskYield();
+	__enable_irq(); 
+
+}
+
+
+
+
+
+
