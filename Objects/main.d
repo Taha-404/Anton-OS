89b969d728a1ec227fce4dfea4a1856c5edc49ca
youssef.taha724@gmail.com <youssef.taha724@gmail.com>
2020-10-24 02:09:22 +0200

    added some final changes

--- Objects/main.d
+++ Objects/main.d
@@ -1,1 +1,13 @@
 .\objects\main.o: main.c
+.\objects\main.o: AntonOS_Kernel.h
+.\objects\main.o: AntonOS_KernelConfig.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C1230H6PM.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: C:\Users\PC\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
+.\objects\main.o: semaphore.h
--- main.c
+++ main.c
@@ -1,3 +1,61 @@
-int main()
+#include "AntonOS_Kernel.h"
+#include"semaphore.h"
+
+
+
+uint32_t count0,count1,count2;
+TaskHandle_t handle0;
+TaskHandle_t handle1;
+TaskHandle_t handle2;
+
+SemaphoreHandle_t semaphore0;
+SemaphoreHandle_t semaphore1;
+
+void Task0(void)
 {
+			SemaphoreTake(&semaphore0,10);
+	while(1)
+	{
+		count0++;
+		TaskBlock(handle0);
+	}
+	
 }
+
+
+void Task1(void)
+{
+			SemaphoreTake(&semaphore0,5);
+	while(1)
+	{
+		count1++;
+		SemaphoreTake(&semaphore0,5);
+	}
+	
+}
+
+
+void Task2(void)
+{
+			SemaphoreGive(&semaphore0);
+	while(1)
+	{
+		count2++;
+	 SemaphoreGive(&semaphore0);
+
+		
+	}
+	
+}
+
+
+int main(void)
+{
+	SemaphoreCreateBinary(&semaphore0);
+	TaskCreate(Task0,3,&handle0);
+	TaskCreate(Task1,2,&handle1);
+	TaskCreate(Task2,1,&handle2);
+	OS_KernelLaunch();
+}
+
+
